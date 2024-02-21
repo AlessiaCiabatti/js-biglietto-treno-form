fca5d7ebe8dca29150995a63c111ae62c9710b1b
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-02-21 16:41:10 +0100

    [add] js-if/else

--- js/script.js
+++ js/script.js
@@ -4,30 +4,32 @@ let chilometriInput = document.querySelector('.chilometri-input');
 let btn1 = document.querySelector('.btn1');
 
 // DROPDOWN
-let dropdown = document.querySelector('.dropdown');
-let junior = document.querySelector('.junior');
-let adults = document.querySelector('.adult');
-let senior = document.querySelector('.senior');
+let choice = document.querySelector('.choice');
 
 
 btn1.addEventListener('click', function(){
   console.log(nomeInput.value);
+  console.log(chilometriInput.value);
   
-  if(dropdown.value === junior.value){
+  if(choice.value === 'junior'){
     let chilometriTotali = Number(chilometriInput.value) * 0.21;
     let prezzoJunior = chilometriTotali - (chilometriTotali * 20 / 100);
     console.log(prezzoJunior.toFixed(2));
-    console.log(dropdown.value);
-    console.log(junior.value);
+    console.log(choice.value);
   }
-  else if(dropdown.value === senior.value){
+
+  else if(choice.value === 'senior'){
     let chilometriTotali = Number(chilometriInput.value) * 0.21;
     let prezzoSenior = chilometriTotali - (chilometriTotali * 40 / 100);
     console.log(prezzoSenior.toFixed(2));
+    console.log(choice.value);
+  }
+
+  else{
+    let chilometriTotali = Number(chilometriInput.value) * 0.21;
+    let prezzoAdults = chilometriTotali;
+    console.log(prezzoAdults.toFixed(2));
+    console.log(choice.value);
   }
 })
 
-
-btn1.addEventListener('click', function(){
-  console.log(chilometriInput.value);
-})
