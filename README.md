129a69437660a9e393ae9925accc63e2d567046e
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-02-21 16:04:17 +0100

    [add] js-jf/elseif

--- README.md
+++ README.md
@@ -13,6 +13,10 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 
 2. con un prompt chiedere il numero di chilometri che vuole percorrere il passeggero e salvarlo in una variabile.
 
-3. abbiamo una const che sono i 0.21 € al km.
+3. chiedere la fascia di età al passeggero.
 
-4. generare il biglietto finale calcolando sulla base dei dati inseriti il prezzo del biglietto.
+4. applicare sconto a seconda dell'età del passeggero.
+
+5. abbiamo una const che sono i 0.21 € al km.
+
+6. generare il biglietto finale calcolando sulla base dei dati inseriti il prezzo del biglietto.
--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
+// INPUT & BTN
 let nomeInput = document.querySelector('.nome-input');
 let chilometriInput = document.querySelector('.chilometri-input');
 let btn1 = document.querySelector('.btn1');
 
+// DROPDOWN
+let dropdown = document.querySelector('.dropdown');
+let junior = document.querySelector('.junior');
+let adults = document.querySelector('.adult');
+let senior = document.querySelector('.senior');
+
+
 btn1.addEventListener('click', function(){
   console.log(nomeInput.value);
+  
+  if(dropdown.value === junior.value){
+    let chilometriTotali = Number(chilometriInput.value) * 0.21;
+    let prezzoJunior = chilometriTotali - (chilometriTotali * 20 / 100);
+    console.log(prezzoJunior.toFixed(2));
+    console.log(dropdown.value);
+    console.log(junior.value);
+  }
+  else if(dropdown.value === senior.value){
+    let chilometriTotali = Number(chilometriInput.value) * 0.21;
+    let prezzoSenior = chilometriTotali - (chilometriTotali * 40 / 100);
+    console.log(prezzoSenior.toFixed(2));
+  }
 })
+
 
 btn1.addEventListener('click', function(){
   console.log(chilometriInput.value);
